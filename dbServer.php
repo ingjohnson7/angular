d160c278603b4596e77ef7b93fae9f2a9bdba8ad
qilobit <ingjohnson7@gmail.com>
2018-02-24 23:31:45 -0400

    adding database actions

---
+++ dbServer.php
@@ -0,0 +1,43 @@
+<?php
+
+if (isset($_POST) && $_POST['action'] != '') {
+
+    try{
+        $con = new PDO('mysql:host=localhost;dbName=angular_p', 'root', '');
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $e)
+    {
+        return "Error: " . $e->getMessage();
+    }
+
+    if($_POST['action'] == 'insert') {
+        $data = cleanFields();
+        try{
+            $query = "INSERT INTO clients VALUES (:name, :lastName, :phone, :registerDate)";    
+            $stm = $con->prepare($query);
+            $stm->bindParam(':name', $data['name']);
+            $stm->bindParam(':lastName', $data['lastName']);
+            $stm->bindParam(':phone', $data['phone']);
+            $stm->bindParam(':registerDate', date('Y-m-d s-i-H'));
+            $stm->execute();
+    
+            return "Client added correctly";
+        }catch(PDOException $e){
+            return "Error saving client: " . $e->getMessage();
+        }
+
+    } else if($_POST['action'] == 'select') {
+
+    }
+
+
+}
+
+function cleanFields() {
+    $data = [];
+    foreach($_POST as $key => $value) {
+        $value = htmlspecialchars(trim($value));
+        $data[$key] = $value;
+    }
+    return $data;
+}
